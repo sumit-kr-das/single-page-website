cbb0a1e893479bacb56204d3b266226c2c49fa62
Sumit Kumar Das <73597446+sumit-kr-das@users.noreply.github.com>
2021-09-06 16:27:46 +0530

    initially creadit section completed

--- script/mainScript.js
+++ script/mainScript.js
@@ -1,9 +1,40 @@
 var counter = 1;
 setInterval(() => {
-    document.getElementById(`radio${counter}`).checked = true;
-    counter++;
+  document.getElementById(`radio${counter}`).checked = true;
+  counter++;
 
-    if(counter > 4){
-        counter = 1;
-    }
+  if (counter > 4) {
+    counter = 1;
+  }
 }, 5000);
+
+// aos Animation timer code
+AOS.init({
+  offset: 300,
+  duration: 1000,
+});
+
+// slick slider code
+$(document).ready(function(){
+  $('.multiple-items').slick({
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+})
