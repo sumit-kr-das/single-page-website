645a40691300c0af12534d7be53050cb4e7ab907
sumit-kr-das <sumitkrdas777@gmail.com>
2021-09-08 01:04:57 +0530

    final committ

--- script/mainScript.js
+++ script/mainScript.js
@@ -73,5 +73,19 @@ $('.slider-nav').slick({
   centerMode: true,
   focusOnSelect: true,
   prevArrow:'.leftArrow',
-  nextArrow:'.rightArrow'
+  nextArrow:'.rightArrow',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 520,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
